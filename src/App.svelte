3f6abea174edf4d53acf76e3cf3109c93f0481de
EliasVal <46247153+EliasVal@users.noreply.github.com>
2021-10-20 23:03:57 +0300

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
+
+<style lang="scss">
+  @import "./styles/index.scss";
+</style>
